ab253439cfe01c25404a0de9cb64f6ba7c9b1d5a
Utkarsh Rai <utkarsh.rai60@gmail.com>
2020-06-09 09:58:59 +0530

    Add test files

---
+++ init.c
@@ -0,0 +1,23 @@
+/*
+ * Simple RTEMS configuration
+ */
+#include<stdlib.h>
+#include<stdbool.h>
+
+void fatal_extension( void);
+
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+
+#define CONFIGURE_UNLIMITED_OBJECTS
+#define CONFIGURE_UNIFIED_WORK_AREAS
+
+#define CONFIGURE_RTEMS_INIT_TASKS_TABLE
+
+#define CONFIGURE_TASK_STACK_ALLOCATOR_INIT  bsp_stack_allocate_init
+#define CONFIGURE_TASK_STACK_ALLOCATOR       bsp_stack_allocate
+#define CONFIGURE_TASK_STACK_DEALLOCATOR     bsp_stack_free
+
+#include <bsp/stackalloc.h>
+#define CONFIGURE_INIT
+#include <rtems/confdefs.h>
